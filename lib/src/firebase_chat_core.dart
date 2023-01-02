b3d49b350fafa73beeb0cbbb40bfb50300e4cd37
Marc Tan <marc.tan@manabie.com>
2023-01-02 16:32:05 +0800

    Add record audio handler

--- lib/src/firebase_chat_core.dart
+++ lib/src/firebase_chat_core.dart
@@ -357,6 +357,12 @@ class FirebaseChatCore {
         id: '',
         partialText: partialMessage,
       );
+    } else if (partialMessage is types.PartialAudio) {
+      message = types.AudioMessage.fromPartial(
+        author: types.User(id: currentUserId),
+        id: '',
+        partialAudio: partialMessage,
+      );
     }
 
     if (message != null) {
